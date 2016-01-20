d4ef3fd97baa60fd659be3b7c647a3d9b69fd923
Sean Foret <foretfl@hotmail.com>
2016-01-19 19:34:33 -0500

    change made from home pc

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
       <title>Foret Florida</title>
    </head>
    <body>
-   <h3>Symfony</h3>
+   <h3>Parking Lot</h3>
    <p>Test site, parking lot for domains and other testing systems, Symfony, PHPTAL, MySQL, etc.</p>
    <a href="taltest.php">Visit the PHPTAL site</a>
    <!--
