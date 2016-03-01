e482e649ddabce3a2d4c84ed8fe194b17f75ea5e
sdforet <sean@foretflorida.com>
2016-03-01 11:41:42 -0500

    site wide spell check, removed a couple of archive links

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
-require_once "php-lib/Decorator.php";
-
-$page = new DECORATOR('index.xhtml');
-$page->css('stylesheets/default');
-$page->title('FFL');
-
-echo $page->execute();
+/**
+ * for now, redirect this page to resume.php
+ */
+   if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+      $uri = 'https://';
+   } else {
+      $uri = 'http://';
+   }
+   $uri .= $_SERVER['HTTP_HOST'];
+   header('Location: '.$uri.'/foretflorida/resume.php');
+   exit;
+/**
+ * require_once "php-lib/Decorator.php";
+ * 
+ * $page = new DECORATOR('index.xhtml');
+ * $page->css('stylesheets/default');
+ * $page->title('FFL');
+ * 
+ * echo $page->execute();
+ */   
