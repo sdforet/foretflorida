b4a836aeb7a6027579bffefe3fd858fccddf4131
Sean Foret <sean@foretflorida.com>
2016-06-29 11:08:36 -0400

    added basic tmdb interface to movies.php

--- movies.php
+++ movies.php
@@ -4,12 +4,15 @@ require_once "php-lib/Decorator.php";
 $page = new DECORATOR('interests/movies.xhtml');
 $page->css(
    'stylesheets/default',
-   'stylesheets/popupMessage'
+   'stylesheets/popupMessage',
+   'stylesheets/movies'
 );
 $page->js(
    'javascript/jquery.cookie',
    'javascript/jquery.slimscroll',
-   'javascript/PopupMessage'
+   'javascript/date',
+   'javascript/PopupMessage',
+   'javascript/movies'
 );
 $page->title('Movies');
 $page->setVarVal('intHL', true);
